b8cb638216f5af4873dd1e97f4fda5b0aec0b602
DiyaS-77 <DIYA.S@CAPGEMINI.COM>
2025-04-29 17:38:30 +0530

    Update pbapmain.py

--- pbapmain.py
+++ pbapmain.py
@@ -5,24 +5,28 @@ bt_manager=BluetoothManager()
 bt_manager.start_discovery()
 bt_manager.Pair(input('Enter the device address to pair : '))
        
-pbap = PhoneBookAccess(input('Enter the device address to connect : '))
-pbap.create_session()
+pbap = PhoneBookAccess()
  
 while True:
       print("\n==== Phonebook Access Menu ====")
-      print("1. Select Phonebook")
-      print("2. Show Phonebook Size")
-      print("3. List Contacts (vCards)")
-      print("4. Pull Single vCard")
-      print("5. Pull Entire Phonebook")
-      print("6. Search Contacts")
-      print("7. Get a Phonebook Property")
-      print("8. List Available Filter Fields")
-      print("9. Disconnect and Exit")
+      print("1. Connect to remote device")
+      print("2. Select Phonebook")
+      print("3. Show Phonebook Size")
+      print("4. List Contacts (vCards)")
+      print("5. Pull Single vCard")
+      print("6. Pull Entire Phonebook")
+      print("7. Search Contacts")
+      print("8. Get a Phonebook Property")
+      print("9. List Available Filter Fields")
+      print("10. Disconnect and Exit")
       print("===============================")
       
       choice = input(" Choose an option (1-9): ")
 
+      if choice == "1":
+         device_bdaddr=input('Enter the bd_addr of the device you want to connect:')
+         pbap.create_session(device_bdaddr)
+             
       if choice == "1":
          user_input=input('Select a repository -- Internal/sim1: ')
          folder=input('Enter which pbap object pb,ich,och,mch,fav,spd:')
@@ -30,22 +34,29 @@ while True:
  
       elif choice == "2":
          pbap.get_size()
+             
       elif choice == "3":
          pbap.list_contacts()
+             
       elif choice == '4':
          handle = input("Enter vCard handle (e.g., 1.vcf): ")
          pbap.pull(handle)
+             
       elif choice == '5':
          pbap.pull_all()
+             
       elif choice == '6':
          searchfield=input('Enter the field for search operation :')
          searchvalue=input('Enter the value name/number/sound: ')
          pbap.search_contacts(searchfield,searchvalue)
+             
       elif choice == '7':
          prop_name=input('Enter the property name :Folder/DatabaseIdentifier/PrimaryCounter/SecondaryCounter/FixedImageSize: ')
          pbap.get_property(prop_name)
+             
       elif choice == '8':
          pbap.list_filters()
+             
       elif choice == '9':
          pbap.disconnect()
          break
